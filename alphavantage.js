70b78861323038d89c2dd4bc479643940338822e
FranklinLiang <franklin.liang@live.com>
2019-01-26 18:51:39 -0500

    Added ability to choose what stock to display.

--- alphavantage.js
+++ alphavantage.js
@@ -1,12 +1,35 @@
-var theURL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=SPY&interval=1min&apikey=6W7XPXYLEGNTFXPV";
-
+var url_base = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=";
+var ticker = "SPY";
+var oldTicker = "SPY";
+var url_end = "&interval=1min&apikey=6W7XPXYLEGNTFXPV";
 $(document).ready(function() {
+
+  // Load ticker
+  if (localStorage.getItem("ticker") != null){
+    ticker = localStorage.getItem("ticker");
+  }else{
+    localStorage.setItem("ticker", "SPY");
+  }
+
+  oldTicker = ticker;
+
+  $("#ticker_button").click(function(){
+    var value = document.getElementById("ticker_input").value;
+    ticker = value;
+    localStorage.setItem("ticker", value);
+    update();
+  });
+
+  update();
+});
+
+function update(){
   $("#stockIndicator").show();
-  doAjax(theURL);
+  doAjax(url_base + ticker + url_end);
 
   $('.ajaxtrigger').click(function() {
     $("#stockIndicator").show();
-    doAjax(theURL);
+    doAjax(url_base + ticker + url_end);
     return false;
   });
 
@@ -21,16 +44,27 @@ $(document).ready(function() {
       contentType: "application/json",
       success: function(data) {
 
-        var symbol = data['Meta Data']['2. Symbol']
-        var lastRefreshed = data['Meta Data']['3. Last Refreshed']
-        var lastTradePriceOnly = data['Time Series (1min)'][lastRefreshed]['4. close']
-        var lastVolume = data['Time Series (1min)'][lastRefreshed]['5. volume']
+        if (!data.hasOwnProperty('Error Message')){
+          var symbol = data['Meta Data']['2. Symbol']
+          var lastRefreshed = data['Meta Data']['3. Last Refreshed']
+          var lastTradePriceOnly = data['Time Series (1min)'][lastRefreshed]['4. close']
+          var lastVolume = data['Time Series (1min)'][lastRefreshed]['5. volume']
 
-        $('#stockSymbol').html(symbol);
-        $('#stockAsk').html(lastTradePriceOnly);
-        $('#stockVolume').html(numberWithCommas(lastVolume));
-        $("#stockIndicator").hide();
+          $('#stockSymbol').html(symbol);
+          $('#stockAsk').html(lastTradePriceOnly);
+          $('#stockVolume').html(numberWithCommas(lastVolume));
+          $("#stockIndicator").hide();
+
+          oldTicker = ticker;
+          
+        }else{
+          alert("Invalid symbol");
+          $("#stockIndicator").hide();
+
+          ticker = oldTicker;
+          localStorage.setItem("ticker", ticker);
+        }
       }
     });
   }
-});
+}
