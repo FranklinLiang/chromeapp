2b94e59baf747eb0e9a9c952dc80db76a8850b2b
FranklinLiang <franklin.liang@live.com>
2019-02-13 13:53:36 -0500

    Fixed datetime.js bugs

--- datetime.js
+++ datetime.js
@@ -16,6 +16,10 @@ function updateClock() {
               {
                 time = now.getHours() + ':' + '0' + now.getMinutes() + ' PM';
               }
+            else if (now.getHours() == 0)
+              {
+                time = (now.getHours() + 12) + ':' + '0' + now.getMinutes() + ' AM';
+              }
             else
               {
                 time = (now.getHours() - 12) + ':' + '0' + now.getMinutes() + ' PM';
@@ -35,6 +39,10 @@ function updateClock() {
               else if (now.getHours() == 24)
                 {
                   time = now.getHours() + ':' + now.getMinutes() + ' PM';
+                }
+              else if (now.getHours() == 0)
+                {
+                  time = (now.getHours() + 12) + ':' + now.getMinutes() + ' AM';
                 }
               else
                 {
